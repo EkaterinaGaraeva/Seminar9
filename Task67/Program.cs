36d43a046e00d23918aee7bc839d0a6db924ce26
Ваше имя <garaevaea@mail.ru>
2022-04-15 03:00:16 +0300

    Add Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 67: Напишите программу, которая будет принимать 
+на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+void SumNumbers(int number, int sum = 0)
+{
+    if (number == 0)
+    {
+        Console.WriteLine($"Сумма цифр = {sum}");
+        return;
+    }
+    else
+    {
+        sum += number % 10;
+        number /= 10;
+        SumNumbers(number, sum);
+    }
+}
+
+SumNumbers(453);
+
